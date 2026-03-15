7d5f991f006fa6ae862ed204cd1036f4024a9f47
gurtbot <gurtbot@gurtbots-MacBook-Air.local>
2026-03-15 06:42:01 +0000

    Fix Supabase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jertmzmngcocfzvprvsb.supabase.co';
-const supabaseAnonKey = 'sb_publishable_hebcWXOhDHNpqPWsCsH_jg_r8Se4dbU';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplcnRtem1uZ2NvY2Z6dnBydnNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MzM3NzMsImV4cCI6MjA4OTEwOTc3M30.TIs0mzZxx3MOWbAKz2j15NlQJ7SjAgksJEVNXDN2LmM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
